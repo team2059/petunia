d13bfefb74fce4315212e3d885a5c2ea2a725d59
Techmj <mrenaaljain@gmail.com>
2022-02-13 15:18:08 -0500

    pid values tuned. fix brake/idle

--- src/main/java/frc/robot/commands/PIDShootCmd.java
+++ src/main/java/frc/robot/commands/PIDShootCmd.java
@@ -21,6 +21,7 @@ public class PIDShootCmd extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
