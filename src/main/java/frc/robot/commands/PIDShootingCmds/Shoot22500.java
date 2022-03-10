14c386c51399b0de2e0be13bcd46af53e95d8e1c
Techmj <mrenaaljain@gmail.com>
2022-03-09 19:19:19 -0500

    variable shooter velocity with game dpad!

--- src/main/java/frc/robot/commands/PIDShootingCmds/Shoot22500.java
+++ src/main/java/frc/robot/commands/PIDShootingCmds/Shoot22500.java
@@ -2,20 +2,20 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
-
+package frc.robot.commands.PIDShootingCmds;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.ShooterSubsystem;
 
-public class PIDShootCmd extends CommandBase {
+public class Shoot22500 extends CommandBase {
   private static ShooterSubsystem mShooter;
   private static double rpmVelocity;
 
   /** Creates a new PIDShootCmd. */
-  public PIDShootCmd(ShooterSubsystem mShooter, double rpmVelocity) {
+  public Shoot22500(ShooterSubsystem mShooter, double rpmVelocity) {
     // Use addRequirements() here to declare submShooter dependencies.
     this.mShooter = mShooter;
     // rpmVelocity in ticks/units 100ms
@@ -47,16 +47,7 @@ public class PIDShootCmd extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    int counter = 0;
 
-    
-
-    // if (RobotContainer.logiGameController.getLeftBumperPressed()) {
-    // counter++;
-    // if (counter % 2 == 0) {
-    // return true;
-    // }
-    // }
     return false;
   }
 }
