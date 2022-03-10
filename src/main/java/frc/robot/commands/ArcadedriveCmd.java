0a979293e0b4f5d53b23c5443c6cbc15cefeec26
Techmj <sundaevlogs@gmail.com>
2022-03-09 19:29:44 -0500

    FINAL DRAFT 1 ~ CODE READY FOR ECU

--- src/main/java/frc/robot/commands/ArcadedriveCmd.java
+++ src/main/java/frc/robot/commands/ArcadedriveCmd.java
@@ -30,8 +30,9 @@ public class ArcadeDriveCmd extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double xSpeed = RobotContainer.logiFlightController.getRawAxis(1) * 0.33;
-    double zRotation = RobotContainer.logiFlightController.getRawAxis(2) * 0.33;
+    double xSpeed = RobotContainer.logiFlightController.getRawAxis(1) * 0.5;
+    double zRotation = RobotContainer.logiFlightController.getRawAxis(2) * 0.5;
+
     // negate zRotation because we invertetd rightMotorControllerGroup
 
     driveTrainSubsystem.arcadeDrive(xSpeed, -zRotation);
