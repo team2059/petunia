875ea6eeb44442b4943150423f5b316675f23989
techmj <sundaevlogs@gmail.com>
2022-04-09 17:12:21 -0400

    shit

--- src/main/java/frc/robot/subsystems/ClimberExtenderSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberExtenderSubsystem.java
@@ -114,7 +114,7 @@ public class ClimberExtenderSubsystem extends SubsystemBase {
 
         @Override
         public void periodic() {
-                SmartDashboard.putNumber("left extend", leftEncoder.getPosition());
-                SmartDashboard.putNumber("right extend", rightEncoder.getPosition());
+                //SmartDashboard.putNumber("left extend", leftEncoder.getPosition());
+                //SmartDashboard.putNumber("right extend", rightEncoder.getPosition());
         }
 }
